f7ce8e3e898becd31fcc73d8dd77e3e9032071c4
talphaidze <talphaidze.ieu2020@student.ie.edu>
2023-07-05 12:43:09 +0200

    navigation from home to product page

--- pages/home.js
+++ pages/home.js
@@ -1,7 +1,4 @@
 import clientPromise from "../lib/mongodb";
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import Sidebar from '../components/Sidebar';
 import { FaSearch, FaTshirt } from 'react-icons/fa';
 
 export default function Products({ products }) {
@@ -10,22 +7,31 @@ export default function Products({ products }) {
       <div className="search-bar">
         <input className="search-input" type="text" placeholder=" Search..." />
         <button className="search-button">
-          <FaSearch /> 
+          <FaSearch />
         </button>
       </div>
 
       <ul className="product-list">
         {products.map((product) => (
           <li key={product._id} className="product-item">
-            <div className="shirt_icon">
-              <FaTshirt color={product.color.hex} /> {/* Use product color */}
-            </div>
-            <h2>{product.name}</h2>
-            <h3>{product.code}</h3>
-            <p>{product.description}</p>
+            <a href={`/products/${product._id}`} className="product-link">
+              <div className="shirt_icon">
+                <FaTshirt color={product.color.hex} />
+              </div>
+              <h2>{product.name}</h2>
+              <h3>{product.code}</h3>
+              <p>{product.description}</p>
+            </a>
           </li>
         ))}
       </ul>
+
+      <style jsx>{`
+        .product-link {
+          text-decoration: none;
+          color: black;
+        }
+      `}</style>
     </div>
   );
 }
