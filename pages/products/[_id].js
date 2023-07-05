5c3f84cc63b300cba215fb4ef4ca355bd3cbaf4c
Rami Pinto <rami.pinto@mongodb.com>
2023-07-05 12:34:57 +0200

    added total stock bar

--- pages/products/[_id].js
+++ pages/products/[_id].js
@@ -7,6 +7,12 @@ import { faShirt } from '@fortawesome/free-solid-svg-icons';
 import styles from '../../styles/product.module.css';
 
 export default function Product({ product }) {
+
+    const totalStockLevel = product.total_stock_sum.find(stock => stock.location === 'store')?.amount ?? 0;
+    const totalStockThreshold = product.total_stock_sum.find(stock => stock.location === 'store')?.threshold ?? 50;
+    const totalProgressBarColor = totalStockLevel >= totalStockThreshold ? 'green' : 'orange';
+    const totalProgressBarFill = (totalStockLevel / 100) * 100;
+    console.log(totalProgressBarFill);
     return (
         <div className={styles['product-detail-content']}>
             <div class="icon">
@@ -17,43 +23,49 @@ export default function Product({ product }) {
                 <p className="code">{product.code}</p>
                 <p className="price">{product.price.amount} {product.price.currency}</p>
             </div>
-        <table className="table">
-            <thead>
-            <tr>
-                <td>Size</td>
-                <td>Store</td>
-                <td>Ordered</td>
-                <td>Warehouse</td>
-                <td>Delivery Time</td>
-                <td>Stock Level</td>
-            </tr>
-            </thead>
-            <tbody>
-            {product.items.map((item, index) => {
-                const stockLevel = item.stock.find(stock => stock.location === 'store')?.amount ?? 0;
-                const stockThreshold = item.stock.find(stock => stock.location === 'store')?.threshold ?? 10;
-                const progressBarColor = stockLevel >= stockThreshold ? 'green' : 'orange';
-                const progressBarFill = (stockLevel / 20) * 100;
-                return (
-                <tr key={index}>
-                <td>{item.size}</td>
-                <td>{item.stock.find(stock => stock.location === 'store')?.amount ?? 0}</td>
-                <td>{item.stock.find(stock => stock.location === 'ordered')?.amount ?? 0}</td>
-                <td>{item.stock.find(stock => stock.location === 'warehouse')?.amount ?? 0}</td>
-                <td>{item.delivery_time.amount}</td>
-                <td>
-                    <div className={styles['progress-bar-container']}>
-                    <div className={styles['progress-bar-reference']}>
-                        <div className={styles['progress-bar-level']} style={{ background: progressBarColor, width: `${progressBarFill}%` }}></div>
-                    </div>
-                    <span className={styles['progress-bar-label']}>20</span>
-                    </div>
-                </td>
+            <table className="table">
+                <thead>
+                <tr>
+                    <td>Size</td>
+                    <td>Store</td>
+                    <td>Ordered</td>
+                    <td>Warehouse</td>
+                    <td>Delivery Time</td>
+                    <td>Stock Level</td>
                 </tr>
-                );
-            })}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                {product.items.map((item, index) => {
+                    const stockLevel = item.stock.find(stock => stock.location === 'store')?.amount ?? 0;
+                    const stockThreshold = item.stock.find(stock => stock.location === 'store')?.threshold ?? 10;
+                    const progressBarColor = stockLevel >= stockThreshold ? 'green' : 'orange';
+                    const progressBarFill = (stockLevel / 20) * 100;
+                    return (
+                    <tr key={index}>
+                    <td>{item.size}</td>
+                    <td>{item.stock.find(stock => stock.location === 'store')?.amount ?? 0}</td>
+                    <td>{item.stock.find(stock => stock.location === 'ordered')?.amount ?? 0}</td>
+                    <td>{item.stock.find(stock => stock.location === 'warehouse')?.amount ?? 0}</td>
+                    <td>{item.delivery_time.amount}</td>
+                    <td>
+                        <div className={styles['progress-bar-container']}>
+                        <div className={styles['progress-bar-reference']}>
+                            <div className={styles['progress-bar-level']} style={{ background: progressBarColor, width: `${progressBarFill}%` }}></div>
+                        </div>
+                        <span className={styles['progress-bar-label']}>20</span>
+                        </div>
+                    </td>
+                    </tr>
+                    );
+                })}
+                </tbody>
+            </table>
+            <div className={styles['progress-bar-container']}>
+                <div className={styles['progress-bar-reference']}>
+                    <div className={styles['progress-bar-level']} style={{ background: totalProgressBarColor, width: `${totalProgressBarFill}%` }}></div>
+                </div>
+                <span className={styles['progress-bar-label']}>100</span>
+            </div>
         </div>
 
     );
