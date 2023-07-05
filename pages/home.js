accd696a8e9c915d77b9ee3e95e6edef07125140
talphaidze <talphaidze.ieu2020@student.ie.edu>
2023-07-05 16:39:54 +0200

    atlas search implementation

--- pages/home.js
+++ pages/home.js
@@ -1,29 +1,67 @@
 import clientPromise from "../lib/mongodb";
+import { useState, useEffect } from 'react';
 import { FaSearch, FaTshirt } from 'react-icons/fa';
 
 export default function Products({ products }) {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = async () => {
+    try {
+      const response = await fetch(`/api/search?q=${searchQuery}`);
+      const data = await response.json();
+
+      if (response.ok) {
+        setSearchResults(data.results);
+      } else {
+        console.error(data.error);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    setSearchResults(products);
+  }, []);
+
   return (
     <div>
       <div className="search-bar">
-        <input className="search-input" type="text" placeholder=" Search..." />
-        <button className="search-button">
+        <input
+          className="search-input"
+          type="text"
+          placeholder=" Search..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          onKeyPress={(e) => {
+            if (e.key === 'Enter') {
+              handleSearch();
+            }
+          }}
+        />
+        <button className="search-button" onClick={handleSearch}>
           <FaSearch />
         </button>
       </div>
 
       <ul className="product-list">
-        {products.map((product) => (
-          <li key={product._id} className="product-item">
-            <a href={`/products/${product._id}`} className="product-link">
-              <div className="shirt_icon">
-                <FaTshirt color={product.color.hex} />
-              </div>
-              <h2>{product.name}</h2>
-              <h3>{product.code}</h3>
-              <p>{product.description}</p>
-            </a>
-          </li>
-        ))}
+        {searchResults.length > 0 ? (
+          searchResults.map((product) => (
+            <li key={product._id} className="product-item">
+              <a href={`/products/${product._id}`} className="product-link">
+                <div className="shirt_icon">
+                  <FaTshirt color={product.color.hex} />
+                </div>
+                <h2>{product.name}</h2>
+                <h3>{product.code}</h3>
+                <p>{product.description}</p>
+              </a>
+            </li>
+          ))
+        ) : (
+          <li>No results found</li>
+        )}
       </ul>
 
       <style jsx>{`
@@ -53,5 +91,8 @@ export async function getServerSideProps() {
     };
   } catch (e) {
     console.error(e);
+    return {
+      props: { products: [] },
+    };
   }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,14 +12,6 @@ export const getServerSideProps: GetServerSideProps<
 > = async () => {
   try {
     await clientPromise
-    // `await clientPromise` will use the default database passed in the MONGODB_URI
-    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
-    //
-    // `const client = await clientPromise`
-    // `const db = client.db("myDatabase")`
-    //
-    // Then you can execute queries against your database like so:
-    // db.find({}) or any of the MongoDB Node Driver commands
 
     return {
       props: { isConnected: true },
