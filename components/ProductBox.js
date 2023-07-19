2c025938218ea21bbfb8a6913cf92c24c38e222b
Rami Pinto <rami.pinto@mongodb.com>
2023-07-19 17:59:17 +0200

    Added alert tags

--- components/ProductBox.js
+++ components/ProductBox.js
@@ -6,6 +6,43 @@ import styles from '../styles/productbox.module.css';
 
 const ProductBox = ({ product }) => { 
 
+    const totalStoreStockSum = product.total_stock_sum.find((stock) => stock.location === 'store');
+    const totalOrderedStockSum = product.total_stock_sum.find((stock) => stock.location === 'ordered');
+
+    const getStockStatus = (storeAmount, orderedAmount, storeThreshold) => {
+        let stockStatus = 'ok';
+
+        if (storeAmount < storeThreshold) {
+            if(storeAmount + orderedAmount > storeThreshold) {
+                stockStatus = 'ordered';
+            }
+            else {
+                stockStatus = 'low';
+            }
+        }
+
+        return stockStatus;
+    };
+
+    const totalStockStatus = getStockStatus(totalStoreStockSum.amount, totalOrderedStockSum.amount, totalStoreStockSum.threshold);
+    let itemStockStatus = 'ok';
+
+    for(const item of product.items) {
+        const itemStoreStockSum = item.stock.find((stock) => stock.location === 'store');
+        const itemOrderedStockSum = item.stock.find((stock) => stock.location === 'ordered');
+
+        let stockStatus = getStockStatus(itemStoreStockSum.amount, itemOrderedStockSum.amount, itemStoreStockSum.threshold);
+
+        if(stockStatus == 'low') {
+            itemStockStatus = 'low';
+            break;
+        } else if(stockStatus == 'ordered') {
+            itemStockStatus = 'ordered';
+        }
+    }
+
+    console.log(product.color.name + ': itemStockStatus: ' + itemStockStatus + ' totalStockStatus: ' + totalStockStatus);
+
 
     return (
         <>
@@ -17,6 +54,13 @@ const ProductBox = ({ product }) => {
                     <h2>{product.name}</h2>
                     <h3>{product.code}</h3>
                     <p>{product.description}</p>
+                    {
+                        itemStockStatus == 'low' || totalStockStatus == 'low' ? 
+                            <span className={`${styles['alert-label']} ${styles['low']}`}>LOW ON STOCK</span> : 
+                            itemStockStatus == 'ordered' || totalStockStatus == 'ordered' ? 
+                                <span className={`${styles['alert-label']} ${styles['ordered']}`}>ORDERED STOCK</span> : 
+                                null
+                    }
                 </a>
             </li>
         </>
