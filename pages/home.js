0b814feb9217dc3fa811cd3ad12bf0d939693b4e
Rami Pinto <rami.pinto@mongodb.com>
2023-07-24 09:57:52 +0200

    Added alerts to home screen

--- pages/home.js
+++ pages/home.js
@@ -1,12 +1,58 @@
 import clientPromise from "../lib/mongodb";
 import { useState, useEffect } from 'react';
-import { FaSearch, FaTshirt } from 'react-icons/fa';
+import  *  as  Realm  from  "realm-web";
+
+import { FaSearch } from 'react-icons/fa';
+
 import Sidebar from '../components/Sidebar';
+import ProductBox from '../components/ProductBox';
+import AlertBanner from '../components/AlertBanner';
 import Fuse from 'fuse.js';
 
+const  app = new  Realm.App({ id:  "interns-mongo-retail-app-nghfn"});
+
 export default function Products({ products, facets }) {
+  
   const [searchQuery, setSearchQuery] = useState('');
   const [filteredProducts, setFilteredProducts] = useState(products);
+  const [alerts, setAlerts] = useState([]);
+
+  useEffect(() => {
+    const  login = async () => {
+        
+      await app.logIn(Realm.Credentials.anonymous());
+      const mongodb = app.currentUser.mongoClient("mongodb-atlas");
+      const collection = mongodb.db("interns_mongo_retail").collection("products");
+      let updatedProduct = null;
+      
+      for await (const  change  of  collection.watch()) {
+        updatedProduct = change.fullDocument;
+        updatedProduct._id = updatedProduct._id.toString();
+
+        setFilteredProducts((prevProducts) =>
+          prevProducts.map((product) =>
+            product._id === updatedProduct._id ? updatedProduct : product
+          )
+        );
+
+        const pattern = /^items\.(\d+)\.stock/;
+        for(const key of Object.keys(change.updateDescription.updatedFields)){
+          if (pattern.test(key)) {
+            let item = updatedProduct.items[parseInt(key.match(pattern)[1], 10)];
+            let itemStoreStock = item.stock.find(stock => stock.location === 'store');
+            
+            if(itemStoreStock.amount < itemStoreStock.threshold) {
+              item.product_id = updatedProduct._id;
+              addAlert(item);
+            }
+          }
+        }
+
+      }
+    }
+    login();
+    handleSearch();
+  }, [searchQuery]);
 
   const handleSearch = () => {
     if (searchQuery.length > 0) {
@@ -29,10 +75,6 @@ export default function Products({ products, facets }) {
     setSearchQuery(searchValue);
   };
 
-  useEffect(() => {
-    handleSearch();
-  }, [searchQuery]);
-
   const filterProducts = (sizesFilter, colorsFilter) => {
     // Filter products based on sizes and colors
     let updatedFilteredProducts = products.filter(product => {
@@ -45,7 +87,22 @@ export default function Products({ products, facets }) {
       return sizeMatch && colorMatch;
     });
     setFilteredProducts(updatedFilteredProducts);
-    console.log('sizes:' + sizesFilter + ' colors:' + colorsFilter + ' products: ' + updatedFilteredProducts.length);
+  };
+
+  const handleAlertClose = (sku) => {
+    setAlerts((prevAlerts) => prevAlerts.filter((alert) => alert.sku !== sku));
+  };
+
+  // Function to add a new alert to the list
+  const addAlert = (item) => {
+
+    setAlerts((prevAlerts) => {
+      if (prevAlerts.some((alert) => alert.sku === item.sku)) {
+        return prevAlerts;
+      } else {
+        return [item, ...prevAlerts].slice(0, 3);
+      }
+    });
   };
 
   return (
@@ -68,28 +125,17 @@ export default function Products({ products, facets }) {
         <ul className="product-list">
           {filteredProducts.length > 0 ? (
             filteredProducts.map((product) => (
-              <li key={product._id} className="product-item">
-                <a href={`/products/${product._id}`} className="product-link">
-                  <div className="shirt_icon">
-                    <FaTshirt color={product.color.hex} />
-                  </div>
-                  <h2>{product.name}</h2>
-                  <h3>{product.code}</h3>
-                  <p>{product.description}</p>
-                </a>
-              </li>
+              <ProductBox key={product._id} product={product}/>
             ))
           ) : (
             <li>No results found</li>
           )}
         </ul>
-
-        <style jsx>{`
-          .product-link {
-            text-decoration: none;
-            color: black;
-          }
-        `}</style>
+      </div>
+      <div className="alert-container">
+        {alerts.map((item) => (
+          <AlertBanner key={item.sku} item={item} onClose={() => handleAlertClose(item.sku)} />
+        ))}
       </div>
     </>
   );
