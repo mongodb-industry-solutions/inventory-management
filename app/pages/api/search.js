cd37d805287c0e5cc71823e8f7de9fe1dd85adc3
RamiPinto <rami.pinto@mongodb.com>
2024-01-29 15:13:11 +0100

    Add edger server folder

---
+++ app/pages/api/search.js
@@ -0,0 +1,72 @@
+import { MongoClient } from 'mongodb';
+import clientPromise from '../../lib/mongodb';
+
+if (!process.env.MONGODB_DATABASE_NAME) {
+  throw new Error('Invalid/Missing environment variables: "MONGODB_DATABASE_NAME"')
+}
+
+const dbName = process.env.MONGODB_DATABASE_NAME;
+const collectionName = 'products';
+
+let cachedDb = null;
+
+async function connectToDatabase() {
+  if (cachedDb) {
+    return cachedDb;
+  }
+
+  const client = await MongoClient.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  const db = client.db(dbName);
+  cachedDb = db;
+
+  return db;
+}
+
+export default async function handler(req, res) {
+  if (req.method !== 'GET') {
+    res.status(405).json({ error: 'Method Not Allowed' });
+    return;
+  }
+
+  const searchQuery = req.query.q;
+
+  if (!searchQuery) {
+    res.status(400).json({ error: 'Search query is required' });
+    return;
+  }
+
+  try {
+    const client = await clientPromise;
+    const db = client.db(dbName);
+
+    const results = await db
+      .collection(collectionName)
+      .aggregate([
+        {
+          $search: {
+            index: 'default',
+            text: {
+              query: searchQuery,
+              path: {
+                wildcard: '*',
+              },
+              fuzzy: {
+                maxEdits: 2, // Adjust the number of maximum edits for typo-tolerance
+              },
+            },
+          },
+        },
+        { $limit: 20 },
+      ])
+      .toArray();
+
+    res.status(200).json({ results });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
