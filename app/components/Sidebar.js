8260609e34113aaa6513e6ffe9dd857371068541
RamiPinto <rami.pinto@mongodb.com>
2024-02-28 15:53:02 +0100

    Fix sidebar filters bug

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -1,61 +1,64 @@
 'use client'
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useContext } from 'react';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import { ServerContext } from '../pages/_app';
 
 import styles from '../styles/sidebar.module.css';
 
 function Sidebar({facets, filterProducts, filterOrders, filterSales, page }) {
 
   const [isShrunk, setIsShrunk] = useState(false);
-  const [selectedSizes, setSelectedSizes] = useState([]);
-  const [selectedColors, setSelectedColors] = useState([]);
+  const [selectedItems, setSelectedItems] = useState([]);
+  const [selectedProducts, setSelectedProducts] = useState([]);
   const [numColorsToShow, setNumColorsToShow] = useState(10);
 
+  const utils = useContext(ServerContext);
 
-  const handleSizeChange = (event) => {
-    const size = event;
-    let updatedSelectedSizes = selectedSizes;
+
+  const handleItemChange = (event) => {
+    const item = event;
+    let updatedSelectedItems = selectedItems;
   
-    if (selectedSizes.includes(size)) {
-      updatedSelectedSizes = selectedSizes.filter((g) => g !== size);
+    if (selectedItems.includes(item)) {
+      updatedSelectedItems = selectedItems.filter((g) => g !== item);
     } else {
-      updatedSelectedSizes = [...selectedSizes, size];
+      updatedSelectedItems = [...selectedItems, item];
     }
-    setSelectedSizes(updatedSelectedSizes);
+    setSelectedItems(updatedSelectedItems);
   
     // Sort the selected sizes according to the desired order
-    const sortedSizes = updatedSelectedSizes.sort((a, b) => {
-      const sizeOrder = ['XS', 'S', 'M', 'L', 'XL'];
-      return sizeOrder.indexOf(a) - sizeOrder.indexOf(b);
+    const sortedItems = updatedSelectedItems.sort((a, b) => {
+      const itemOrder = ['XS', 'S', 'M', 'L', 'XL'];
+      return itemOrder.indexOf(a) - itemOrder.indexOf(b);
     });
   
     if (page === 'products') {
-      filterProducts(sortedSizes, selectedColors);
+      filterProducts(sortedItems, selectedProducts);
     } else if (page === 'orders') {
-      filterOrders(sortedSizes, selectedColors);
+      filterOrders(sortedItems, selectedProducts);
     } else if (page === 'sales') {
-      filterSales(sortedSizes, selectedColors);  {/* Call filterSales for sales page */}
+      filterSales(sortedItems, selectedProducts);  {/* Call filterSales for sales page */}
     }
       };
     
-      const handleColorChange = (event) => {
+      const handleProductChange = (event) => {
         const color = event;
-        let updatedSelectedColors = selectedColors;
+        let updatedSelectedProducts = selectedProducts;
 
-        if (selectedColors.includes(color)) {
-          updatedSelectedColors = selectedColors.filter((y) => y !== color);
-          setSelectedColors(updatedSelectedColors);
+        if (selectedProducts.includes(color)) {
+          updatedSelectedProducts = selectedProducts.filter((y) => y !== color);
+          setSelectedProducts(updatedSelectedProducts);
         } else {
-          updatedSelectedColors = [...selectedColors, color];
-            setSelectedColors(updatedSelectedColors);
+          updatedSelectedProducts = [...selectedProducts, color];
+            setSelectedProducts(updatedSelectedProducts);
         }
         if (page === 'products') {
-          filterProducts(selectedSizes, updatedSelectedColors);
+          filterProducts(selectedItems, updatedSelectedProducts);
         } else if (page === 'orders') {
-          filterOrders(selectedSizes, updatedSelectedColors);
+          filterOrders(selectedItems, updatedSelectedProducts);
         } else if (page === 'sales') {
-          filterSales(selectedSizes, updatedSelectedColors);  {/* Call filterSales for sales page */}
+          filterSales(selectedItems, updatedSelectedProducts);  {/* Call filterSales for sales page */}
         }
       };
 
@@ -81,18 +84,23 @@ function Sidebar({facets, filterProducts, filterOrders, filterSales, page }) {
       {!isShrunk && (
         <>
           <div className={styles["size-filters"]} >
-            <h3>Items</h3>
-            {facets?.[0]?.facet?.sizesFacet.buckets
+            <h3>
+            { utils.demoInfo.industry == 'manufacturing' ? 
+                "Items" : 
+                "Size"
+              }
+            </h3>
+            {facets?.[0]?.facet?.itemsFacet.buckets
   .sort((a, b) => {
-    const sizeOrder = ['XS', 'S', 'M', 'L', 'XL'];
-    return sizeOrder.indexOf(a._id) - sizeOrder.indexOf(b._id);
+    const itemOrder = ['XS', 'S', 'M', 'L', 'XL'];
+    return itemOrder.indexOf(a._id) - itemOrder.indexOf(b._id);
   })
   .map((bucket) => (
     <label key={bucket._id}>
       <input
         type="checkbox"
-        checked={selectedSizes.includes(bucket._id)}
-        onChange={() => handleSizeChange(bucket._id)}
+        checked={selectedItems.includes(bucket._id)}
+        onChange={() => handleItemChange(bucket._id)}
       />
       <span>{bucket._id}</span>
     </label>
@@ -100,23 +108,28 @@ function Sidebar({facets, filterProducts, filterOrders, filterSales, page }) {
           </div>
 
           <div className={styles["color-filters"]} >
-            <h3>Products</h3>
+            <h3>
+              { utils.demoInfo.industry == 'manufacturing' ? 
+                "Category" : 
+                "Color"
+              }
+            </h3>
             <div className={styles["color-list"]}>
-              {facets?.[0]?.facet?.colorsFacet.buckets.slice(0, numColorsToShow).map((bucket) => (
+              {facets?.[0]?.facet?.productsFacet.buckets.slice(0, numColorsToShow).map((bucket) => (
                 <label key={bucket._id}>
                   <input
                     type="checkbox"
-                    checked={selectedColors.includes(bucket._id)}
-                    onChange={() => handleColorChange(bucket._id)}
+                    checked={selectedProducts.includes(bucket._id)}
+                    onChange={() => handleProductChange(bucket._id)}
                   />
                   <span>{bucket._id}</span>
                 </label>
               ))}
-              {numColorsToShow < facets?.[0]?.facet?.colorsFacet.buckets.length && (
+              {numColorsToShow < facets?.[0]?.facet?.productsFacet.buckets.length && (
                 <button onClick={handleExpand}>Show More</button>
               )}
 
-              {numColorsToShow >= facets?.[0]?.facet?.colorsFacet.buckets.length && facets[0].facet.colorsFacet.buckets.length > 10  && (
+              {numColorsToShow >= facets?.[0]?.facet?.productsFacet.buckets.length && facets[0].facet.productsFacet.buckets.length > 10  && (
                 <button onClick={handleCollapse}>Show Less</button>
               )}
             </div>
